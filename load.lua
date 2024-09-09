72f02400f5865dbae0e456a4bf986e8cda69bbfa
Cpt. Hazama <Caleb123459@gmail.com>
2024-09-09 17:43:53 -0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -4,15 +4,17 @@ FGOA.Network.BuildID = "00001"
 FGOA.Network.BannerID = "0001"
 FGOA.Network.News = {}
 
-local function AddNews(title, description, image, dates)
+local function AddNews(title, description, image, dates, link)
     local news = {}
     news.title = title
     news.description = description
     news.image = image
     news.dates = dates
+    news.link = link
 
     table.insert(FGOA.Network.News, news)
 end
 
 AddNews("News System Test: Start","This is the first news system test.",nil,"09/08/2024")
 AddNews("News System Test: Complete","The news system is completely tested! Players can now receive live news feed updates in-game.",nil,"09/09/2024")
+AddNews("News System Test: Links","This is a test to see if the links can be properly opened.",nil,"09/09/2024","https://www.youtube.com/watch?v=gkTb9GP9lVI")
