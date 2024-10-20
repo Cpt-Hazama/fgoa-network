f4c8e9a2059518b826adf975cc2f354d3651538f
Cpt. Hazama <Caleb123459@gmail.com>
2024-10-20 01:30:37 -0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -4,13 +4,14 @@ FGOA.Network.BuildID = "00001"
 FGOA.Network.BannerID = "0001"
 FGOA.Network.News = {}
 
-local function AddNews(title, description, image, dates, link)
+local function AddNews(title, description, image, dates, link, banner)
     local news = {}
     news.title = title
     news.description = description
     news.image = image
     news.dates = dates
     news.link = link
+    news.banner = banner or nil
 
     table.insert(FGOA.Network.News, news)
 end
@@ -18,5 +19,5 @@ end
 AddNews("News System Test: Start","This is the first news system test.",nil,"09/08/2024")
 AddNews("News System Test: Complete","The news system is completely tested! Players can now receive live news feed updates in-game.",nil,"09/09/2024")
 AddNews("News System Test: Links","This is a test to see if the links can be properly opened.",nil,"09/09/2024","https://www.youtube.com/watch?v=gkTb9GP9lVI")
-AddNews("New Banner: Test Summons!","A new banner featuring 'Artoria Pendragon (Saber)', 'Artoria Pendragon (Alter-Saber)' and 'Morgan le Fay (Berserker)' is now available!","fgoa/sprite/ui/panel_bg_summon1.png","09/09/2024","https://docs.google.com/document/d/1PQWAkZ4iZnQcP4yeDdxuXsntMOMO1f-AynoYnx6POjE/edit?usp=sharing")
-AddNews("New Banner: Test Summons 2!","A new banner featuring 'Artoria Pendragon (Saber)', 'Artoria Pendragon (Alter-Saber)' and 'Morgan le Fay (Berserker)' is now available!","fgoa/sprite/ui/panel_bg_summon1.png","09/30/2024","https://docs.google.com/document/d/1PQWAkZ4iZnQcP4yeDdxuXsntMOMO1f-AynoYnx6POjE/edit?usp=sharing")
+AddNews("New Banner: Test Summons!","A new banner featuring 'Artoria Pendragon (Saber)', 'Artoria Pendragon (Alter-Saber)' and 'Morgan le Fay (Berserker)' is now available!","fgoa/sprite/ui/panel_bg_summon1.png","09/09/2024","https://docs.google.com/document/d/1PQWAkZ4iZnQcP4yeDdxuXsntMOMO1f-AynoYnx6POjE/edit?usp=sharing","0001")
+AddNews("New Banner: Test Summons 2!","A new banner featuring 'Artoria Pendragon (Saber)', 'Artoria Pendragon (Alter-Saber)' and 'Morgan le Fay (Berserker)' is now available!","fgoa/sprite/ui/panel_bg_summon1.png","09/30/2024","https://docs.google.com/document/d/1PQWAkZ4iZnQcP4yeDdxuXsntMOMO1f-AynoYnx6POjE/edit?usp=sharing","0001")
